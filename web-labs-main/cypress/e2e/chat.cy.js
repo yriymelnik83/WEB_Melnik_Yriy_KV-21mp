fc0e5f4d8c48281af3e14b724bca87d403219b45
yriymelnik83 <71213759+yriymelnik83@users.noreply.github.com>
2023-06-25 16:49:57 +0300

    Delete web-labs-main directory

--- web-labs-main/cypress/e2e/chat.cy.js
+++
@@ -1,8 +0,0 @@
-describe('chat spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:8000')
-    cy.get('input[id="messageText"]').type('Test')
-    cy.get('button[id="chatButton"]').click()
-    cy.get('textArea[id="chatArea"]').should('have.value','anonymus : Test\n')
-  })
-})
--- web-labs-main/shortlinks/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ShortlinksConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'shortlinks'
